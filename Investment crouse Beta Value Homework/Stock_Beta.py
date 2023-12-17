ba686d85409b287d95e30bc35f0298818f4c9d86
秋禾秋火 <99186207+choice0980@users.noreply.github.com>
2023-12-18 00:31:54 +0800

    方便使用函數

--- Investment crouse Beta Value Homework/Stock_Beta.py
+++ Investment crouse Beta Value Homework/Stock_Beta.py
@@ -1,19 +1,22 @@
 import numpy as np
 
-def stock_beta_and_totalRisk(stock_closing_prices, twii_closing_prices):
-    # 任意股票XXXXX (XXXXX.TW) 的收盤價數據 , 加權指數 (^TWII) 的收盤價數據
+def stock_beta_and_totalRisk(stock_closing_prices, index_closing_prices):
 
     # 計算日收益率
-    twii_returns = np.diff(twii_closing_prices) / twii_closing_prices[:-1]
+    index_returns = np.diff(index_closing_prices) / index_closing_prices[:-1]
     stock_returns = np.diff(stock_closing_prices) / stock_closing_prices[:-1]
 
     # 計算共變異數和市場變異數
-    covariance = np.cov(stock_returns, twii_returns)[0, 1]
-    market_variance = np.var(twii_returns)
+    covariance = np.cov(stock_returns, index_returns)[0, 1]
+    market_variance = np.var(index_returns)
 
     # 計算 Beta 值
     beta = covariance / market_variance
     total_risk = np.std(stock_returns)
 
     print('This sotck\'s Beta: ', beta, '\nThis stock\'s total risk: ', total_risk)
+
+stock_price = np.array([10, 11, 12, 13, 14])
+index_price = np.array([100, 110, 120, 130, 140])
+stock_beta_and_totalRisk(stock_price,index_price )
 
